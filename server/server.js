6052d45569c00da82c76ee55eac9f7d41d987ae0
Misaal <misaal9@gmail.com>
2017-05-01 19:08:09 +0530

    Set up basic messaging app

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, (req, res) => {
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('createMsg', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      msg: message.msg,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client has disconnected');
+  });
+});
+
+server.listen(port, (req, res) => {
   console.log(`Listening to port ${port}`);
 });
