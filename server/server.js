5f21422e58bb5e7f25ea96b026c9cfb2c8ebdbe4
Misaal <misaal9@gmail.com>
2017-05-02 00:20:15 +0530

    Generate message from generator

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {generateMessage} = require('./util/message');
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
@@ -17,21 +18,13 @@ io.on('connection', (socket) => {
   console.log('New client connected');
 
   // welcome new user to group
-  socket.emit('welcomeUser', {
-    text: "[Admin] Welcome to the group chat"
-  });
+  socket.emit('welcomeUser', generateMessage('Admin', 'Welcome to the chat'));
 
   // notify all of new user added
-  socket.broadcast.emit('newUserAdd', {
-    text: "[Admin] New user has been added"
-  });
+  socket.broadcast.emit('newUserAdd', generateMessage('Admin', 'New user has been added'));
 
   socket.on('createMsg', (message) => {
-    socket.broadcast.emit('newMessage', {
-      from: message.from,
-      msg: message.msg,
-      createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
